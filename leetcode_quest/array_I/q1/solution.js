3ffeb8d641e2aeffe3853e208deaab3e76a2e4e2
Hami Ibriyamov <hamop3108@gmail.com>
2026-04-23 18:59:02 +0200

    Add getConcatenation function in solution.js
    Implement getConcatenation function to return concatenated array.

---
+++ leetcode_quest/array_I/q1/solution.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    ars = []
+    nums_clone = nums
+    ars = nums.concat(nums_clone)
+    return ars
+};
